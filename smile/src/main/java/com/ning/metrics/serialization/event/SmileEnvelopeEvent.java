aae4e5fa67ff3fdf3306936d9156c1ac68a6796b
Tatu <tatu@ning.com>
2010-11-04 16:24:14 -0700

    Improvements to Smile envelope, to allow better reuse

--- smile/src/main/java/com/ning/metrics/serialization/event/SmileEnvelopeEvent.java
+++ smile/src/main/java/com/ning/metrics/serialization/event/SmileEnvelopeEvent.java
@@ -14,9 +14,13 @@ import java.nio.charset.Charset;
 
 public class SmileEnvelopeEvent implements Event
 {
-    private final Charset CHARSET_LATIN1 = Charset.forName("ISO-8859-1");
+    /**
+     * We will use bogus charset to do truncation from char[] to byte[]; latin-1
+     * works since it is 8-bit subset of Unicode.
+     */
+    protected final static Charset CHARSET_LATIN1 = Charset.forName("ISO-8859-1");
     
-    private final static SmileFactory factory = new SmileFactory();
+    protected final static SmileFactory factory = new SmileFactory();
     static {
         // yes, full 'compression' by checking for repeating names, short string values:
         factory.configure(SmileGenerator.Feature.CHECK_SHARED_NAMES, true);
@@ -25,15 +29,13 @@ public class SmileEnvelopeEvent implements Event
         factory.configure(SmileParser.Feature.REQUIRE_HEADER, false);
     }
 
-    private JsonParser parser;
-
     public static final String SMILE_EVENT_DATETIME_TOKEN_NAME = "eventDate";
     public static final String SMILE_EVENT_GRANULARITY_TOKEN_NAME = "eventGranularity";
 
-    private DateTime eventDateTime = null;
-    private String eventName;
-    private Granularity granularity = null;
-    private byte[] payload;
+    protected DateTime eventDateTime = null;
+    protected String eventName;
+    protected Granularity granularity = null;
+    protected byte[] payload;
 
     @Deprecated
     public SmileEnvelopeEvent()
@@ -50,13 +52,41 @@ public class SmileEnvelopeEvent implements Event
     public SmileEnvelopeEvent(String eventName, String message) throws IOException
     {
         this.eventName = eventName;
-        this.payload = message.getBytes(CHARSET_LATIN1);
-        parser = factory.createJsonParser(payload);
-        parseEvent();
+        parseEvent(bytesFromString(message));
     }
 
-    private void parseEvent() throws IOException
+    public SmileEnvelopeEvent(String eventName, byte[] payload) throws IOException
     {
+        this.eventName = eventName;
+        this.eventDateTime = null;
+        parseEvent(payload);
+    }
+
+    /**
+     * Constructor that will not (have to) parse payload, but is instead directly
+     * given metadata that accompanies (and should be included in) serialized payload
+     */
+    public SmileEnvelopeEvent(String eventName, byte[] payload,
+            DateTime eventDateTime, Granularity granularity)
+    {
+        this.eventName = eventName;
+        this.payload = payload;
+        this.eventDateTime = eventDateTime;
+        this.granularity = granularity;
+    }
+    
+    private void parseEvent(byte[] payload) throws IOException
+    {
+        if (payload == null) {
+            throw new IllegalArgumentException("Missing payload for parseEvent()");
+        }
+        
+        this.payload = payload;
+        granularity = null;
+        eventDateTime = null;
+
+        JsonParser parser = factory.createJsonParser(payload);
+
         // Go through the stream once
         JsonToken t = parser.nextToken();
         while (t != null && (eventDateTime == null || granularity == null)) {
@@ -87,10 +117,10 @@ public class SmileEnvelopeEvent implements Event
                 t = parser.nextToken();
             }
         }
-
         if (granularity == null) {
             granularity = Granularity.HOURLY;
         }
+        parser.close();
     }
 
     @Override
@@ -168,16 +198,13 @@ public class SmileEnvelopeEvent implements Event
      *                                restored cannot be found.
      */
     @Override
-    public void readExternal(ObjectInput in) throws IOException, ClassNotFoundException
+    public void readExternal(ObjectInput in) throws IOException
     {
         int numBytes = in.readInt();
         byte[] bytes = new byte[numBytes];
 
         in.readFully(bytes);
-
-        payload = bytes;
-        parser = factory.createJsonParser(bytes);
-        parseEvent();
+        parseEvent(payload);
         // One has to set the name manually
     }
 
@@ -195,5 +222,21 @@ public class SmileEnvelopeEvent implements Event
             ", granularity=" + granularity +
             ", payloadLength=" + payload.length +
             '}';
+    }
+
+    /*
+    /////////////////////////////////////////////////////////////////////// 
+    // Helper methods for sub-classes
+    /////////////////////////////////////////////////////////////////////// 
+     */
+
+    protected static byte[] bytesFromString(String str)
+    { 
+        return str.getBytes(CHARSET_LATIN1); 
+    }
+
+    protected String stringFromBytes(byte[] raw)
+    {
+        return new String(raw, CHARSET_LATIN1);
     }
 }
