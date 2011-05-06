f49fa3257260897c789107f3cc0dfa88a6ba6eee
Pierre-Alexandre Meyer <pierre@ning.com>
2011-05-06 14:12:13 -0700

    writer: add note for expected exception in tests
    Signed-off-by: Pierre-Alexandre Meyer <pierre@ning.com>

--- writer/src/test/java/com/ning/metrics/serialization/writer/TestDiskSpoolEventWriter.java
+++ writer/src/test/java/com/ning/metrics/serialization/writer/TestDiskSpoolEventWriter.java
@@ -20,11 +20,13 @@ import com.ning.metrics.serialization.event.Event;
 import com.ning.metrics.serialization.event.StubEvent;
 import org.apache.log4j.Logger;
 import org.testng.Assert;
-import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
-import java.io.*;
+import java.io.File;
+import java.io.FileFilter;
+import java.io.IOException;
+import java.io.ObjectOutput;
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
@@ -195,7 +197,7 @@ public class TestDiskSpoolEventWriter
             {
                 if (!hasFailedOneEvent) {
                     hasFailedOneEvent = true;
-                    throw new IOException("Failing an event!");
+                    throw new IOException("IGNORE - Expected exception for tests");
                 }
             }
         }));
