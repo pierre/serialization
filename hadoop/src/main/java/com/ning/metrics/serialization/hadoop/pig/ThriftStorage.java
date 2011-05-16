d79d29b9ec0485b8104e0f044113a96543ac253c
Pierre-Alexandre Meyer <pierre@ning.com>
2011-05-16 11:26:37 -0700

    hadoop: add Goodwill support for pig storage
    Signed-off-by: Pierre-Alexandre Meyer <pierre@ning.com>

--- hadoop/src/main/java/com/ning/metrics/serialization/hadoop/pig/ThriftStorage.java
+++ hadoop/src/main/java/com/ning/metrics/serialization/hadoop/pig/ThriftStorage.java
@@ -17,17 +17,23 @@ import org.apache.hadoop.mapreduce.RecordReader;
 import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
 import org.apache.hadoop.mapreduce.lib.input.SequenceFileInputFormat;
 import org.apache.log4j.Logger;
+import org.apache.pig.Expression;
 import org.apache.pig.LoadFunc;
+import org.apache.pig.LoadMetadata;
+import org.apache.pig.ResourceSchema;
+import org.apache.pig.ResourceStatistics;
 import org.apache.pig.backend.hadoop.executionengine.mapReduceLayer.PigSplit;
+import org.apache.pig.data.DataType;
 import org.apache.pig.data.Tuple;
 import org.apache.pig.data.TupleFactory;
+import org.apache.pig.impl.logicalLayer.schema.Schema;
 
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.concurrent.ExecutionException;
 
-public class ThriftStorage extends LoadFunc
+public class ThriftStorage extends LoadFunc implements LoadMetadata
 {
     private static final Logger log = Logger.getLogger(ThriftStorage.class);
 
@@ -194,5 +200,107 @@ public class ThriftStorage extends LoadFunc
             default:
                 return dataItem.getString();
         }
+    }
+
+    private byte getPigType(SchemaFieldType type)
+    {
+        switch (type) {
+            case BOOLEAN:
+                return DataType.INTEGER;
+            case BYTE:
+                return DataType.BYTE;
+            case SHORT:
+            case INTEGER:
+                return DataType.INTEGER;
+            case LONG:
+                return DataType.LONG;
+            case DOUBLE:
+                return DataType.DOUBLE;
+            case DATE:
+            case IP:
+            case STRING:
+            default:
+                return DataType.CHARARRAY;
+        }
+    }
+
+    /**
+     * Get a schema for the data to be loaded.
+     *
+     * @param location Location as returned by
+     *                 {@link org.apache.pig.LoadFunc#relativeToAbsolutePath(String, org.apache.hadoop.fs.Path)}
+     * @param job      The {@link org.apache.hadoop.mapreduce.Job} object - this should be used only to obtain
+     *                 cluster properties through {@link org.apache.hadoop.mapreduce.Job#getConfiguration()} and not to set/query
+     *                 any runtime job information.
+     * @return schema for the data to be loaded. This schema should represent
+     *         all tuples of the returned data.  If the schema is unknown or it is
+     *         not possible to return a schema that represents all returned data,
+     *         then null should be returned. The schema should not be affected by pushProjection, ie.
+     *         getSchema should always return the original schema even after pushProjection
+     * @throws java.io.IOException if an exception occurs while determining the schema
+     */
+    @Override
+    public ResourceSchema getSchema(String location, Job job) throws IOException
+    {
+        List<Schema.FieldSchema> schemaList = new ArrayList<Schema.FieldSchema>();
+        for (GoodwillSchemaField field : schema.getSchema()) {
+            schemaList.add(new Schema.FieldSchema(field.getName(), getPigType(field.getType())));
+        }
+
+        return new ResourceSchema(new Schema(schemaList));
+    }
+
+    /**
+     * Get statistics about the data to be loaded.  If no statistics are
+     * available, then null should be returned.
+     *
+     * @param location Location as returned by
+     *                 {@link org.apache.pig.LoadFunc#relativeToAbsolutePath(String, org.apache.hadoop.fs.Path)}
+     * @param job      The {@link org.apache.hadoop.mapreduce.Job} object - this should be used only to obtain
+     *                 cluster properties through {@link org.apache.hadoop.mapreduce.Job#getConfiguration()} and not to set/query
+     *                 any runtime job information.
+     * @return statistics about the data to be loaded.  If no statistics are
+     *         available, then null should be returned.
+     * @throws java.io.IOException if an exception occurs while retrieving statistics
+     */
+    @Override
+    public ResourceStatistics getStatistics(String location, Job job) throws IOException
+    {
+        return null;
+    }
+
+    /**
+     * Find what columns are partition keys for this input.
+     *
+     * @param location Location as returned by
+     *                 {@link org.apache.pig.LoadFunc#relativeToAbsolutePath(String, org.apache.hadoop.fs.Path)}
+     * @param job      The {@link org.apache.hadoop.mapreduce.Job} object - this should be used only to obtain
+     *                 cluster properties through {@link org.apache.hadoop.mapreduce.Job#getConfiguration()} and not to set/query
+     *                 any runtime job information.
+     * @return array of field names of the partition keys. Implementations
+     *         should return null to indicate that there are no partition keys
+     * @throws java.io.IOException if an exception occurs while retrieving partition keys
+     */
+    @Override
+    public String[] getPartitionKeys(String location, Job job) throws IOException
+    {
+        return null;
+    }
+
+    /**
+     * Set the filter for partitioning.  It is assumed that this filter
+     * will only contain references to fields given as partition keys in
+     * getPartitionKeys. So if the implementation returns null in
+     * {@link #getPartitionKeys(String, org.apache.hadoop.mapreduce.Job)}, then this method is not
+     * called by Pig runtime. This method is also not called by the Pig runtime
+     * if there are no partition filter conditions.
+     *
+     * @param partitionFilter that describes filter for partitioning
+     * @throws java.io.IOException if the filter is not compatible with the storage
+     *                             mechanism or contains non-partition fields.
+     */
+    @Override
+    public void setPartitionFilter(Expression partitionFilter) throws IOException
+    {
     }
 }
